8a28f545cdcd8482dc24bfd4de70024d577a23ee
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-02-20 20:21:55 +0700

    fix bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
  
+
+// Import Component Clock
+import ContainerClock from "./assets/component/ContainerClock"
+
+
 function App() {
 
   return (
     <section className="App">
-      <div className="main-content  bg-[url('../images/mobile/bg-image-nighttime.jpg')] md:bg-[url('../images/tablet/bg-image-nighttime.jpg')] lg:bg-[url('../images/desktop/bg-image-nighttime.jpg')]  before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-custom1 before:opacity-[0.5]">
-        
-    
+      <div className="main-content font-inter  bg-[url('../images/mobile/bg-image-nighttime.jpg')] md:bg-[url('../images/tablet/bg-image-nighttime.jpg')] lg:bg-[url('../images/desktop/bg-image-nighttime.jpg')]  before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-custom1 before:opacity-[0.4] lg:before:opacity-[0.4] contrast-[1.1] brightness-[]">
+
+      {/* Component Clock */}
+      <ContainerClock/>
+
       </div>
     </section>
   )
---
+++ src/assets/component/ContainerClock.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const ContainerClock = ()=>{
+    return(
+        <section className="container-clock border-2 border-red-600">
+            {/* container quotes */}
+            <section className="container-quotes w-full h-[100%] border-2 flex-1">
+
+            
+            </section>
+            <div className="clock border-2 border-cyan-700 w-full h-[50px]">
+
+            </div>
+        </section>
+    )
+}
+
+export default ContainerClock 
