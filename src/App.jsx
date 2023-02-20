47dfd18647681a61e8c04dc247631987ab9d79e3
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-02-20 19:10:32 +0700

    install pkg npm

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <section className="App">
-      <div className="main-content relative w-full h-[100vh] bg-[url('../images/mobile/bg-image-nighttime.jpg')] md:bg-[url('../images/tablet/bg-image-nighttime.jpg')] lg:bg-[url('../images/desktop/bg-image-nighttime.jpg')]  bg-no-repeat bg-cover bg-center scale-[1.01] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-custom1 before:opacity-[0.4]">
+      <div className="main-content  bg-[url('../images/mobile/bg-image-nighttime.jpg')] md:bg-[url('../images/tablet/bg-image-nighttime.jpg')] lg:bg-[url('../images/desktop/bg-image-nighttime.jpg')]  before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-custom1 before:opacity-[0.4]">
         
     
       </div>
