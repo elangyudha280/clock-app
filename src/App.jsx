5c5145946d7aebd5f20bc002f0bedf030c4c27f6
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-02-20 19:20:36 +0700

    mulai desain component container clock

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <section className="App">
-      <div className="main-content  bg-[url('../images/mobile/bg-image-nighttime.jpg')] md:bg-[url('../images/tablet/bg-image-nighttime.jpg')] lg:bg-[url('../images/desktop/bg-image-nighttime.jpg')]  before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-custom1 before:opacity-[0.4]">
+      <div className="main-content  bg-[url('../images/mobile/bg-image-nighttime.jpg')] md:bg-[url('../images/tablet/bg-image-nighttime.jpg')] lg:bg-[url('../images/desktop/bg-image-nighttime.jpg')]  before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-custom1 before:opacity-[0.5]">
         
     
       </div>
