b4d2b492f56f33fded32c2512bfc0620886041a8
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-02-26 10:56:45 +0700

    fix font title quotes

--- src/component/componentContainerClock/Quotes.jsx
+++ src/component/componentContainerClock/Quotes.jsx
@@ -8,7 +8,7 @@ const Quotes = ()=>{
         <section className="container-quotes w-full h-[100%]  flex-1">
             <div className="quotes max-w-xl  flex gap-2">
                 <div className="description-quotes flex-1 leading-[1.7em]">
-                    <p className="text-white font-[300] text-[0.8em] md:text-[1.1em] ">
+                    <p className="text-white font-[300] text-[0.8em] md:text-[0.9em] ">
                             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque doloribus officia esse voluptates quibusdam id dolores reiciendis blanditiis quia in."
                     </p>
                     <h3 className="quote character text-white font-bold text-xl capitalize mt-3">jon doe</h3>
