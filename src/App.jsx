41b8a39c7c232adae702d5b24ab6b0e62cee6a07
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-02-27 19:04:33 +0700

    mulai fitur toggle zone

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <section className="App  font-inter  bg-[url('../images/mobile/bg-image-daytime.jpg')]  xs:bg-[url('../images/desktop/bg-image-daytime.jpg')]  before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-custom1 before:opacity-[0.5] contrast-[1.1] ">
-      <div className="main-content">
+      <div className="main-content group">
       {/* Component Clock */}
       <ContainerClock/>
       </div>
